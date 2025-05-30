72dcc9e6e8112b67a01a9241232dfeac71267e9d
wrteamjanki <wrteam.janki1510@gmail.com>
2025-05-30 09:36:21 +0530

    Update router.py

--- app/income/router.py
+++ app/income/router.py
@@ -11,46 +11,23 @@ from app.income.service import (
     update_income,
     get_income_categories,
 )
-<<<<<<< HEAD
-=======
-
-from app.auth.dependencies import get_current_user
-
->>>>>>> 58f2f395c5250be0b665bc36d0f6748b7045e8b2
 
 router = APIRouter(prefix="/income", tags=["Income"])
 
 
 @router.post("/", response_model=IncomeEntry)
-<<<<<<< HEAD
 async def create_income(entry: IncomeCreate):
-=======
 
-async def create_income(entry: IncomeCreate, token: str = Depends(get_current_user)):
-
->>>>>>> 58f2f395c5250be0b665bc36d0f6748b7045e8b2
     return await add_income(entry)
 
 
 @router.get("/", response_model=List[IncomeEntry])
-<<<<<<< HEAD
 async def read_all_income():
-=======
-
-async def read_all_income(token: str = Depends(get_current_user)):
-
->>>>>>> 58f2f395c5250be0b665bc36d0f6748b7045e8b2
     return await get_all_income()
 
 
 @router.delete("/{income_id}", response_model=dict)
-<<<<<<< HEAD
 async def remove_income(income_id: int):
-=======
-
-async def remove_income(income_id: int, token: str = Depends(get_current_user)):
-
->>>>>>> 58f2f395c5250be0b665bc36d0f6748b7045e8b2
     success = await delete_income(income_id)
 
     if not success:
@@ -61,12 +38,7 @@ async def remove_income(income_id: int, token: str = Depends(get_current_user)):
 
 
 @router.put("/{income_id}", response_model=IncomeEntry)
-<<<<<<< HEAD
 async def edit_income(income_id: int, entry: IncomeUpdate):
-=======
-
-async def edit_income(income_id: int, entry: IncomeUpdate, token: str = Depends(get_current_user)):
->>>>>>> 58f2f395c5250be0b665bc36d0f6748b7045e8b2
     try:
 
         return await update_income(income_id, entry)
@@ -77,12 +49,7 @@ async def edit_income(income_id: int, entry: IncomeUpdate, token: str = Depends(
 
 
 @router.get("/categories", response_model=List[str])
-<<<<<<< HEAD
 async def income_categories():
-=======
 
-async def income_categories(token: str = Depends(get_current_user)):
-
->>>>>>> 58f2f395c5250be0b665bc36d0f6748b7045e8b2
     return await get_income_categories()
 
